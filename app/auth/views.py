6c71e4ac3243cf52eaa234b390f501fae89f748e
Ghabriel Villarreal <ghabrielv@gmail.com>
2020-11-10 16:02:04 -0500

    Login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,12 @@
+from app.models import UserModel
 from flask import render_template, session, redirect, flash, url_for
+from flask_login import login_user, login_required, logout_user
 
 from app.forms import LoginForm
 
 from . import auth
+from app.firestore_service import get_user
+from app.models import UserData, UserModel
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -14,10 +18,35 @@ def login():
 
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
+        password = login_form.password.data
 
-        flash('Successfully logged in.')
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+
+                flash('Welcome back')
+
+                redirect(url_for('hello'))
+            else:
+                flash('Incorrect username/password')
+        else:
+            flash('Incorrect username/password')
 
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Come back soon')
+
+    return redirect(url_for('auth.login'))
