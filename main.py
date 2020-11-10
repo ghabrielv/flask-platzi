d5129481bd390209919bcf656470b5867f470ad7
Ghabriel Villarreal <ghabrielv@gmail.com>
2020-11-10 09:32:37 -0500

    Refactor

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from app import create_app
+from app.forms import LoginForm
 
-app.config['SECRET_KEY'] = 'SUPER SECRET'
+app = create_app()
 
 todos = ['Buy coffee', 'Send request', 'Send video']
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
 
 @app.cli.command()
 def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner().run(tests)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
 
+
 @app.errorhandler(500)
 def not_found(error):
     return render_template('500.html', error=error)
+
 
 @app.route('/')
 def index():
@@ -59,4 +54,5 @@ def hello():
 
         return redirect(url_for('index'))
 
-    return render_template('hello.html', **context) # NOTE: ** expand dictionary
+    # NOTE: ** expand dictionary
+    return render_template('hello.html', **context)
